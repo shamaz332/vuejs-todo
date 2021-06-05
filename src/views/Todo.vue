0fa5b51c553a09cc93c6362937dee62b87bee969
shamaz332 <shamazsaeed332@gmail.com>
2021-06-05 21:32:38 +0500

    toggle todos

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,10 +1,56 @@
 <template>
-    <h1 class="pa-6">Todo</h1>
+  <div class="pa-6">
+    <v-list class="pt-0" flat>
+      <v-list-item-group v-model="settings" multiple>
+        <div v-for="task in tasks" :key="task.id">
+          <v-list-item @click="doneTask(task.id)">
+            <template v-slot:default>
+              <v-list-item-action>
+                <v-checkbox
+                  :input-value="task.done"
+                  color="primary"
+                ></v-checkbox>
+              </v-list-item-action>
+
+              <v-list-item-content>
+                <v-list-item-title>{{ task.title }}</v-list-item-title>
+              </v-list-item-content>
+            </template>
+          </v-list-item>
+          <v-divider></v-divider>
+        </div>
+      </v-list-item-group>
+    </v-list>
+  </div>
 </template>
 
 
 <script>
 export default {
-  name:'Todo'
-}
+  name: "Todo",
+  data() {
+    return {
+      tasks: [
+        {
+          id: "1",
+          title: "Monday Gym",
+          done: true,
+        },
+        {
+          id: "2",
+          title: "Tuesday Gym",
+          done: false,
+        },
+      ],
+    };
+  },
+  methods:{
+    doneTask(id){
+      console.log(id)
+      let task = this.tasks.filter(task => task.id = id)[0]
+
+      task.done = !task.done
+    }
+  }
+};
 </script>
